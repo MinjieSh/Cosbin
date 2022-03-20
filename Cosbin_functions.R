b3d7217a287b2b022f6f834a682f866545142dae
MinjieSh <mjshen809@gmail.com>
2022-03-20 00:36:55 -0500

    Evaluation refactor

--- Cosbin_functions.R
+++ Cosbin_functions.R
@@ -214,3 +214,159 @@ plot_ternary_data <-
     
     dev.off()
   }
+
+########################## evaluation functions
+# CEG dislocation: cosine_dislocation(data_grnd, CEG_grnd, norm_data_list)
+# DEG dislocation (exclude sDEG): cosine_dislocation(data_grnd, DEG_grnd, norm_data_list)
+cosine_dislocation <- function(data_grnd, grnd_label, norm_data_list){
+  
+  number_of_methods <- length(norm_data_list)
+  ind <- which(grnd_label == 1)
+  table <- matrix(0, nrow = number_of_methods, ncol = 4)
+  
+  for(j in 1:number_of_methods){
+    norm_data <- norm_data_list[[j]]
+    
+    res <- NULL
+    
+    for (i in ind) {
+      ref <- data_grnd[i, ]
+      res <- c(res, mycosine(ref, norm_data[i,]))
+    }
+    
+    # hist(res, xlim = c(0.85, 1), breaks = seq(0.85, 1, 0.001))
+    table[j, 1] <- mean(res)
+    table[j, 2] <- (180 * acos(Mean(res))) / pi
+    table[j, 3] <- (180 * acos(min(res))) / pi
+    table[j, 4] <- (180 * acos(max(res))) / pi
+  }
+  return(table)
+}
+
+# sDEG dislocation: groupwise_cosine_dislocation(data_grnd, norm_data_list, nGroup, pSMG)
+groupwise_cosine_dislocation <- function(data_grnd, norm_data_list, nGroup, pSMG){
+  
+  number_of_methods <- length(norm_data_list)
+  table <- array(0, c(number_of_methods, nGroup, 4))
+  
+  for(k in 1:number_of_methods){
+    norm_data <- norm_data_list[[k]]
+    
+    for (j in 1:nGroup) {
+      res <- NULL
+      
+      start <- 1
+      for (i in start : (start + pSMG[j] - 1)) {
+        ref <- data_grnd[i, ]
+        res <- c(res, mycosine(ref, norm_data[i,]))
+      }
+      start <- start + (pSMG[j]* nGene)
+      
+      table[k, j, 1] <- mean(res)
+      table[k, j, 2] <- (180 * acos(Mean(res))) / pi
+      table[k, j, 3] <- (180 * acos(min(res))) / pi
+      table[k, j, 4] <- (180 * acos(max(res))) / pi
+    }
+  }
+  return(table)
+}
+
+# ROC (iCEG vs non-iCEG): ROC(norm_data_list, CEG_grnd, "iCEG", c("red", "green", "blue", "orange", "magenta"))
+# ROC (DEG (exclude SMG) vs non DEG (exclude SMG)) : ROC(norm_data_list, DEG_grnd, "DEG", c("red", "green", "blue", "orange", "magenta"))
+ROC <- function(norm_data_list, grnd_label, option = 'iCEG', color_list) {
+  number_of_methods <- length(norm_data_list)
+  
+  TPR_list <- list()
+  FPR_list <- list()
+  AUC_list <- list()
+  
+  for (i in 1:number_of_methods) {
+    if (option == "iCEG") {
+      lst <-
+        sort(cos_iCEG(norm_data_list[[i]]),
+             index.return = TRUE,
+             decreasing = TRUE)$ix
+      P <- sum(grnd_label)
+      N <- nGene - P
+      limit <- nGene
+      threshold <- 0.15
+    } else {
+      lst <-
+        sort(cos_iDEG(norm_data_list[[i]]),
+             index.return = TRUE,
+             decreasing = TRUE)$ix
+      lst <- lst[lst > sum(pSMG)]
+      P <- sum(grnd_label)
+      N <- nGene - sum(pSMG) - P
+      limit <- N + P
+      threshold <- 0.8
+    }
+    
+    res <- NULL
+    TPR <- NULL
+    FPR <- NULL
+    res <- NULL
+    
+    for (j in 1:limit) {
+      res <- c(res, sum(grnd_label[lst[1:j]]))
+      TP <- sum(grnd_label[lst[1:j]] == 1)
+      FP <- sum(grnd_label[lst[1:j]] == 0)
+      TPR <- c(TPR, TP / P)
+      FPR <- c(FPR, FP / N)
+    }
+    TPR_list[[i]] <- TPR
+    FPR_list[[i]] <- FPR
+    AUC_list[[i]] <-
+      1 - (1 - DescTools::AUC(FPR, TPR)) / threshold # depending on the results
+  }
+  
+  plot(
+    FPR_list[[1]],
+    TPR_list[[1]],
+    xlab = "FPR",
+    ylab = "TPR",
+    xlim = c(0, threshold),
+    ylim = c(0, 1),
+    type = "l",
+    lty = 2,
+    col = color_list[1]
+  )
+  
+  
+  for (i in 2:length(norm_data_list)) {
+    lines(FPR_list[[i]], TPR_list[[i]], lty = 2, col = color_list[i])
+  }
+  
+  
+  legend(
+    "bottomright",
+    legend = c("Total count", "Cosbin", "TMM", "DESeq2", "TCC"),
+    col = color_list,
+    lty = rep(2, length(norm_data_list)),
+    cex = 0.8
+  )
+  
+  return(list(FPR_list, TPR_list, AUC_list))
+}
+
+# average_of_pairwise_Log_Fold_Change_Mean_Squared_Error: average_of_pairwise_LFC_MSE(norm_data_list, CEG_grnd, nGroup)
+average_of_pairwise_LFC_MSE <- function(norm_data_list, grnd_label, nGroup){
+  number_of_methods <- length(norm_data_list)
+  
+  ind <- which(grnd_label == 1)
+  table <- matrix(0, nrow = number_of_methods, ncol = 1)
+  
+  for(k in 1:number_of_methods){
+    norm_data <- norm_data_list[[k]]
+    res <- NULL
+    for (i in 1:(nGroup - 1)) {
+      for (j in (i + 1):nGroup) {
+        normCounts1 <- norm_data[ind, i]
+        normCounts2 <- norm_data[ind, j]
+        res <- c(res, mean((log(normCounts1 / normCounts2) - log(1/1)) ^ 2))
+      }
+    }
+    table[k,] <- mean(res)
+  }
+  return(table)
+}
--- evaluation.R
+++ evaluation.R
@@ -1,221 +1,26 @@
-outlis <- list(totalcount_out, 
-               cosbin_out$data, 
-               tmm_out, 
-               DESeq2_out, 
-               tcc_out)
+norm_data_list <- list(totalcount_out,
+                       cosbin_out$data,
+                       tmm_out,
+                       DESeq2_out,
+                       tcc_out)
 
-
-i <- 2
-plot_data <- outlis[[i]]
 
 ##################################################################
 
-# CEG Cosine Dislocation
-ind <- which(CEG_grnd == 1)
-table <- matrix(0, nrow = length(outlis), ncol = 4)
+# CEG dislocation: 
+CEG_dislocation <- cosine_dislocation(data_grnd, CEG_grnd, norm_data_list)
 
-for(j in 1:length(outlis)){
-  plot_data <- outlis[[j]]
-  
-  res <- NULL
-  
-  nGroups <- 3
-  for (i in ind) {
-    ref <- data_grnd[i, ]
-    res <-
-      c(res, sum(ref * plot_data[i,]) / sqrt(sum(ref ^ 2) * sum(plot_data[i,] ^ 2)))
-  }
-  # hist(res, xlim = c(0.85, 1), breaks = seq(0.85, 1, 0.001))
-  table[j, 1] <- mean(res)
-  table[j, 2] <- (180 * acos(Mean(res))) / pi
-  table[j, 3] <- (180 * acos(min(res))) / pi
-  table[j, 4] <- (180 * acos(max(res))) / pi
-}
+# DEG dislocation (exclude sDEG): 
+DEG_dislocation <- cosine_dislocation(data_grnd, DEG_grnd, norm_data_list)
 
-########################################################
+# sDEG dislocation: 
+sDEG_dislocation <- groupwise_cosine_dislocation(data_grnd, norm_data_list, nGroup, pSMG)
 
-# cosine non-iCEG (exclude sDEG) displacement
+# ROC (iCEG vs non-iCEG): 
+ROC_iCEG <- ROC(norm_data_list, CEG_grnd, "iCEG", c("#AA000066", "#00AA0066", "#0000AA66",  "#AAAA0066", "#00AAAA66"))
 
-ind <- which(DEG_grnd == 1)
-table <- matrix(0, nrow = length(outlis), ncol = 4)
+# ROC (DEG (exclude SMG) vs non DEG (exclude SMG)): 
+ROC_DEG <- ROC(norm_data_list, DEG_grnd, "DEG", c("#AA000066", "#00AA0066", "#0000AA66",  "#AAAA0066", "#00AAAA66"))
 
-result <- NULL
-for(j in 1:length(outlis)){
-  plot_data <- outlis[[j]]
-  res <- NULL
-  nGroups <- 3
-  for (i in ind) {
-    ref <- data_grnd[i, ]
-    res <-
-      c(res, sum(ref * plot_data[i,]) / sqrt(sum(ref ^ 2) * sum(plot_data[i,] ^ 2)))
-  }
-  # hist(res, xlim = c(0.85, 1), breaks = seq(0.85, 1, 0.001))
-  table[j, 1] <- mean(res)
-  table[j, 2] <- (180 * acos(Mean(res))) / pi
-  table[j, 3] <- (180 * acos(min(res))) / pi
-  table[j, 4] <- (180 * acos(max(res))) / pi
-}
-
-#########################################################
-
-# cosine sDEG displacement
-table <- array(0, c(length(outlis), nGroup, 4))
-
-for(k in 1:length(outlis)){
-  plot_data <- outlis[[k]]
-  
-  for (j in 1:nGroup) {
-    res <- NULL
-    start <- 1
-    for (i in start : (start + pSMG[j] - 1)) {
-      res <-
-        c(res, sum(data_grnd[i, ] * plot_data[i,]) / sqrt(sum(data_grnd[i, ]  ^ 2) * sum(plot_data[i,] ^ 2)))
-    }
-    start <- start + (pSMG[j]* nGene)
-    
-    table[k, j, 1] <- mean(res)
-    table[k, j, 2] <- (180 * acos(Mean(res))) / pi
-    table[k, j, 3] <- (180 * acos(min(res))) / pi
-    table[k, j, 4] <- (180 * acos(max(res))) / pi
-  }
-}
-
-##############################
-
-# ROC (iCEG vs non-iCEG)
-
-TPRlis <- list()
-FPRlis <- list()
-AUClis <- list()
-for(i in 1:length(outlis)){
-  lst <- sort(cos_iCEG(outlis[[i]]), index.return = TRUE, decreasing = TRUE)
-  res <- NULL
-  P <- sum(CEG_grnd)
-  N <- nGene - P
-  TPR <- NULL
-  FPR <- NULL
-  res <- NULL
-  for (j in 1:nGene) {
-    res <- c(res, sum(CEG_grnd[lst$ix[1:j]]))
-    TP <- sum(CEG_grnd[lst$ix[1:j]] == 1)
-    FP <- sum(CEG_grnd[lst$ix[1:j]] == 0)
-    TPR <- c(TPR, TP / P)
-    FPR <- c(FPR, FP / N)
-  }
-  TPRlis[[i]] <- TPR
-  FPRlis[[i]] <- FPR
-  AUClis[[i]] <- 1 - (1 - DescTools::AUC(FPR, TPR)) / 0.15
-}
-
-colorlis <- c("red", "green", "blue", "orange", "magenta")
-
-plot(
-  FPRlis[[1]],
-  TPRlis[[1]],
-  xlab = "FPR",
-  ylab = "TPR",
-  xlim = c(0, 0.15),
-  ylim = c(0, 1),
-  type= "l",
-  lty = 2,
-  col = colorlis[1]
-)
-
-
-for(i in 2:length(outlis)){
-  lines(FPRlis[[i]], TPRlis[[i]], lty=2, col = colorlis[i])
-}
-
-
-legend("bottomright",
-       legend = c("Total count", "Cosbin", "TMM", "DESeq2",
-                  "TCC"),
-       col= colorlis,
-       lty=rep(2, length(outlis)), cex=0.8)
-
-#########################################################
-
-# ROC (DEG (exclude SMG) vs non DEG (exclude SMG))
-
-TPRlis <- list()
-FPRlis <- list()
-AUClis <- list()
-
-for(i in 1:length(outlis)){
-  temp <- outlis[[i]]
-  lst <- sort(cos_iDEG(temp), 
-              index.return = TRUE, decreasing = TRUE)$ix
-  lst <- lst[lst > sum(pSMG)]
-  
-  res <- NULL
-  P <- sum(DEG_grnd)
-  N <- nGene - sum(pSMG) - P 
-  TPR <- NULL
-  FPR <- NULL
-  res <- NULL
-  for (j in 1:(N+P)) {
-    res <- c(res, sum(DEG_grnd[lst[1:j]]))
-    TP <- sum(DEG_grnd[lst[1:j]] == 1)
-    FP <- sum(DEG_grnd[lst[1:j]] == 0)
-    TPR <- c(TPR, TP / P)
-    FPR <- c(FPR, FP / N)
-  }
-  TPRlis[[i]] <- TPR
-  FPRlis[[i]] <- FPR
-  AUClis[[i]] <- 1 - (1 - DescTools::AUC(FPR, TPR)) / 0.8
-}
-
-colorlis <- c("red", "green", "blue", "orange", "pink")
-
-plot(
-  FPRlis[[1]],
-  TPRlis[[1]],
-  xlab = "FPR",
-  ylab = "TPR",
-  xlim = c(0, 1),
-  ylim = c(0, 1),
-  type= "l",
-  lty = 2,
-  col = colorlis[1]
-)
-
-
-for(i in 2:length(outlis)){
-  lines(FPRlis[[i]], TPRlis[[i]], lty=2, col = colorlis[i])
-}
-
-
-legend("bottomright", 
-       legend = c("Total count", "Cosbin", "TMM", "DESeq2",
-                  "TCC"),
-       col= colorlis,
-       lty=rep(2, length(outlis)), cex=0.8)
-
-########################################################
-
-
-
-###########################################################
-
-# average of pairwise LFC MSE
-
-ind <- which(CEG_grnd == 1)
-table <- matrix(0, nrow = length(outlis), ncol = 1)
-
-for(k in 1:length(outlis)){
-  plot_data <- outlis[[k]]
-  res <- NULL
-  nGroups <- 3
-  for (i in 1:(nGroups - 1)) {
-    for (j in (i + 1):nGroups) {
-      normCounts1 <- plot_data[ind, i]
-      normCounts2 <- plot_data[ind, j]
-      res <- c(res, mean(log(normCounts1 / normCounts2) ^ 2))
-    }
-  }
-  table[k,] <- mean(res)
-}
-
-###########################################################
-
-save.image(file='642021-2.RData')
+# average_of_pairwise_Log_Fold_Change_Mean_Squared_Error: 
+mean_LFC_MSE <- average_of_pairwise_LFC_MSE(norm_data_list, CEG_grnd, nGroup)
