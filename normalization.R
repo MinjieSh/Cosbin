eae83296f59865e6e5244e6d5a28b009af99f040
MinjieSh <mjshen809@gmail.com>
2022-03-20 22:29:34 -0500

    Update normalization.R

--- normalization.R
+++ normalization.R
@@ -1,37 +1,44 @@
-# super sample
+
+#################################################### (group mean)
+
+input_data <- data3
+nGroup <- 3
+
+#### Cosbin normaliztaion (group mean)
 start.time <- Sys.time()
-cosbin_out <- cosbin(data3)
+cosbin_out <- cosbin(input_data)
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
 
-# normaliztaion
-totalcount_out <- totalcount(data3)$data
+
+#### Total count normaliztaion (group mean)
+totalcount_out <- totalcount(input_data)$data
 
 
-# DESeq2 super sample
+#### DESeq2 (group mean)
 cond <- factor(1:nGroup)
-data3_int <- round(data3)
-dds <- DESeqDataSetFromMatrix(data3_int, DataFrame(cond), ~ cond)
+input_data_int <- round(input_data)
+dds <- DESeqDataSetFromMatrix(input_data_int, DataFrame(cond), ~ cond)
 dds <- estimateSizeFactors(dds)
 sizeFactors(dds) <- sizeFactors(dds)/geometric.mean(sizeFactors(dds))
-DESeq2_out <- data3
+DESeq2_out <- input_data
 for(i in 1:nGroup){
   DESeq2_out[, i] <- DESeq2_out[, i] / sizeFactors(dds)[i]
 }
 
 
-#### TMM/edgeR
-normFactors <- edgeR::calcNormFactors(data3)
+#### TMM/edgeR (group mean)
+normFactors <- edgeR::calcNormFactors(input_data)
 normFactors <- normFactors/geometric.mean(normFactors)
-tmm_out <- data3
+tmm_out <- input_data
 for(i in 1:nGroup){
   tmm_out[, i] <- tmm_out[, i] / normFactors[i]
 }
 
 
-# TCC with replicates
-tcc <- new("TCC", data3, 1:nGroup)
+#### TCC (group mean)
+tcc <- new("TCC", input_data, 1:nGroup)
 tcc <-
   calcNormFactors(
     tcc,
@@ -44,94 +51,98 @@ tcc <-
 tccEsts <- tcc$norm.factors
 tccEsts <- tccEsts / geometric.mean(tccEsts)
 
-tcc_out <- data3
+tcc_out <- input_data
 for(i in 1:nGroup){
   tcc_out[, i] <- tcc_out[, i] / tccEsts[i]
 }
 
-####################################################
+#################################################### (with replicates, then average to evaluate)
 
-# # DESeq2 duplicate
-# cond <- factor(rep(1:nGroup, nRep))
-# data2_int <- round(data2)
-# dds <- DESeqDataSetFromMatrix(data2_int, DataFrame(cond), ~ cond)
-# dds <- estimateSizeFactors(dds)
-# sizeFactors(dds) <- sizeFactors(dds)/geometric.mean(sizeFactors(dds))
-# DESeq2_temp <- data2
-# for(i in 1:sum(nRep)){
-#   DESeq2_temp[, i] <- DESeq2_temp[, i] / sizeFactors(dds)[i]
-# }
-# DESeq2_out <- NULL
-# start <- 1
-# for(rep in nRep){
-#   DESeq2_out <- cbind(DESeq2_out, rowMeans(DESeq2_temp[, start : (start + rep - 1)]))
-#   start <- start + rep
-# }
+input_data2 <- data2
+nGroup <- 3
+nRep <- c(10, 10, 10)
 
-# edgeR normalization - TMM
-# normFactors <- edgeR::calcNormFactors(data2)
-# totCounts <- colSums(data2)
-# normFactors <- normFactors * totCounts
-# normFactors <- normFactors/geometric.mean(normFactors)
-# tmm_temp <- data2
-# for(i in 1:sum(nRep)){
-#   tmm_temp[, i] <- tmm_temp[, i] / normFactors[i]
-# }
-# tmm_out <- NULL
-# start <- 1
-# for(rep in nRep){
-#   tmm_out <- cbind(tmm_out, rowMeans(tmm_temp[, start : (start + rep - 1)]))
-#   start <- start + rep
-# }
-# cor(normFactors, factor)
+#### DESeq2 (with replicates, then average to evaluate)
+cond <- factor(rep(1:nGroup, nRep))
+input_data2_int <- round(input_data2)
+dds <- DESeqDataSetFromMatrix(input_data2_int, DataFrame(cond), ~ cond)
+dds <- estimateSizeFactors(dds)
+sizeFactors(dds) <- sizeFactors(dds)/geometric.mean(sizeFactors(dds))
+DESeq2_temp <- input_data2
+for(i in 1:sum(nRep)){
+  DESeq2_temp[, i] <- DESeq2_temp[, i] / sizeFactors(dds)[i]
+}
 
-#
-# tcc <- new("TCC", data3, 1:nGroup)
-# tcc <-
-#   TCC::calcNormFactors(
-#     tcc,
-#     norm.method = "tmm",
-#     test.method = "wad",
-#     iteration = 1,
-#     FDR = 0.1,
-#     floorPDEG = 0.05
-#   )
-# tccEsts <- tcc$norm.factors
-# totCounts <- colSums(data3)
-# tccEsts <- tccEsts * totCounts
-# tccEsts <- tccEsts / geometric.mean(tccEsts)
-# 
-# tcc_out <- getNormalizedData(tcc)
+## get average for evaluation
+DESeq2_out <- NULL
+start <- 1
+for(rep in nRep){
+  DESeq2_out <- cbind(DESeq2_out, rowMeans(DESeq2_temp[, start : (start + rep - 1)]))
+  start <- start + rep
+}
+
+#### edgeR normalization - TMM (with replicates, then average to evaluate)
+normFactors <- edgeR::calcNormFactors(input_data2)
+totCounts <- colSums(input_data2)
+normFactors <- normFactors * totCounts
+normFactors <- normFactors/geometric.mean(normFactors)
+tmm_temp <- input_data2
+for(i in 1:sum(nRep)){
+  tmm_temp[, i] <- tmm_temp[, i] / normFactors[i]
+}
+
+## get average for evaluation
+tmm_out <- NULL
+start <- 1
+for(rep in nRep){
+  tmm_out <- cbind(tmm_out, rowMeans(tmm_temp[, start : (start + rep - 1)]))
+  start <- start + rep
+}
+
+#### TCC (group mean, different test.method)
+tcc <- new("TCC", input_data, 1:nGroup)
+tcc <-
+  TCC::calcNormFactors(
+    tcc,
+    norm.method = "tmm",
+    test.method = "wad",
+    iteration = 1,
+    FDR = 0.1,
+    floorPDEG = 0.05
+  )
+tccEsts <- tcc$norm.factors
+totCounts <- colSums(input_data)
+tccEsts <- tccEsts * totCounts
+tccEsts <- tccEsts / geometric.mean(tccEsts)
+
+tcc_out <- getNormalizedData(tcc)
 
 
-# TCC with replicates
-# tcc <- new("TCC", data2, rep(1:nGroup, each = 10))
-# tcc <-
-#   calcNormFactors(
-#     tcc,
-#     norm.method = "tmm",
-#     test.method = "edger",
-#     iteration = 1,
-#     FDR = 0.1,
-#     floorPDEG = 0.05
-#   )
-# tccEsts <- tcc$norm.factors
-# totCounts <- colSums(data2)
-# tccEsts <- tccEsts * totCounts
-# tccEsts <- tccEsts / geometric.mean(tccEsts)
-# 
-# # tcc_temp <- getNormalizedData(tcc)
-# 
-# tcc_temp <- data2
-# for(i in 1:sum(nRep)){
-#   tcc_temp[, i] <- tcc_temp[, i] / tccEsts[i]
-# }
-# 
-# tcc_out <- NULL
-# start <- 1
-# for(rep in nRep){
-#   tcc_out <- cbind(tcc_out, rowMeans(tcc_temp[, start : (start + rep - 1)]))
-#   start <- start + rep
-# }
-# 
-# cor(tccEsts, factor)
+#### TCC (with replicates, then average to evaluate)
+tcc <- new("TCC", input_data2, rep(1:nGroup, each = 10))
+tcc <-
+  calcNormFactors(
+    tcc,
+    norm.method = "tmm",
+    test.method = "edger",
+    iteration = 1,
+    FDR = 0.1,
+    floorPDEG = 0.05
+  )
+tccEsts <- tcc$norm.factors
+totCounts <- colSums(input_data2)
+tccEsts <- tccEsts * totCounts
+tccEsts <- tccEsts / geometric.mean(tccEsts)
+
+tcc_temp <- input_data2
+for(i in 1:sum(nRep)){
+  tcc_temp[, i] <- tcc_temp[, i] / tccEsts[i]
+}
+
+## get average for evaluation
+tcc_out <- NULL
+start <- 1
+for(rep in nRep){
+  tcc_out <- cbind(tcc_out, rowMeans(tcc_temp[, start : (start + rep - 1)]))
+  start <- start + rep
+}
