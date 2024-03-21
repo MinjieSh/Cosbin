3c3f8d0764bf7016e1c2f53451223279737604f1
Dongping Du <89655676+niccolodpdu@users.noreply.github.com>
2024-03-20 22:18:08 -0400

    Update Example.R

--- Example.R
+++ Example.R
@@ -33,23 +33,23 @@ nGene <- 824 # number of genes
 
 nRep <- c(4,3,3) # number of samples in each group
 
-data2 <- totalcount(data)$data # firstly use total-count method to normalize the original data
+data_tc <- totalcount(data)$data # firstly use total-count method to normalize the original data
 
 
-## Generating Super sample
-data3 <- NULL
+## Generating Super Sample (basically the group mean)
+data_ss <- NULL
 start <- 1
 for(rep in nRep){
-  data3 <- cbind(data3, rowMeans(data2[, start : (start + rep - 1)])) 
-  # data 3 will have a column number the same as your group number (3 in this example)
+  data_ss <- cbind(data_ss, rowMeans(data_tc[, start : (start + rep - 1)])) 
+  # data_ss will have a column number the same as your group number (3 in this example)
   start <- start + rep
 }
 
 ## Cosbin functions
-cosbin_out <- cosbin(data3)
-# cosbin_out carries the group-specific weights
+cosbin_out <- cosbin(data_ss)
+# cosbin_out carries the group-specific weights/scalers
 
-cosbin_out_full <- cosbin_convert(cosbin_out, data2)
+cosbin_out_full <- cosbin_convert(cosbin_out, data_tc)
 # cosbin_out_full carries the cosbin-normalized data
 
 write.csv(cosbin_out_full,'cosbin_normed.csv')
